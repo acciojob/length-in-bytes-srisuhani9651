e9508382ec5954c33ef08dec8e8be559e99b2652
srisuhani9651 <srisuhani9651@gmail.com>
2026-07-14 13:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const byteSize = (str) => {
   // write your code here
+	const blob = new Blob(str)
+	return blob.size
 };
+
 
 // Do not change the code below
 const str = prompt("Enter some string.");
